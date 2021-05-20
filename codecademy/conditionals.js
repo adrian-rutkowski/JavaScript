78cce67d4bfebe246aed07bb1cc6bc6be2a6fbd2
adrian-rutkowski <adrnrtkvski@gmail.com>
2021-05-20 16:14:54 +0200

    update

---
+++ codecademy/conditionals.js
@@ -0,0 +1,92 @@
+let cond1 = true;
+if (cond1) {
+    console.log('This message will print!'); 
+}
+// Prints: This message will print!
+
+let cond2 = false;
+if (cond2) {
+ console.log('The code in this block will not run.');
+} else {
+console.log('But the code in this block will!');
+}
+// Prints: But the code in this block will!
+
+/*
+Logical operators:
+and (&&)
+or  (||)
+not (!)
+*/
+let stopLight = 'green';
+let pedestrians = '0';
+if (stopLight === 'green' && pedestrians === 0) {
+    console.log('Go!');
+} else {
+    console.log('Stop');
+}
+
+let mood = 'sleepy';
+let tirednessLevel = 6;
+if (mood === 'sleepy' || tirednessLevel > 8) {
+  console.log('time to sleep');
+} else {
+  console.log('not bed time yet');
+}
+
+// Truthy value
+let myVariable = 'I Exist!';
+if (myVariable) {
+   console.log(myVariable)
+} else {
+   console.log('The variable does not exist.')
+}
+
+// Falsy value
+let numberOfApples = 0;
+if (numberOfApples){
+   console.log('Let us eat apples!');
+} else {
+   console.log('No apples left!');
+}
+// Prints 'No apples left!'
+
+// Truthy assignment
+let username = '';
+let defaultName = username || 'Stranger';
+
+// Ternary operator
+let favoritePhrase = 'Love That!';
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+
+// Else If statements
+let stopLight = 'yellow';
+if (stopLight === 'red') {
+  console.log('Stop!');
+} else if (stopLight === 'yellow') {
+  console.log('Slow down.');
+} else if (stopLight === 'green') {
+  console.log('Go!');
+} else {
+  console.log('Caution, unknown!');
+}
+
+// Switch keyword
+let groceryItem = 'papaya';
+ 
+switch (groceryItem) {
+  case 'tomato':
+    console.log('Tomatoes are $0.49');
+    break;
+  case 'lime':
+    console.log('Limes are $1.49');
+    break;
+  case 'papaya':
+    console.log('Papayas are $1.29');
+    break;
+  default:
+    console.log('Invalid item');
+    break;
+}
+// Prints 'Papayas are $1.29'
+
--- codecademy/introduction.js
+++ codecademy/introduction.js
@@ -24,3 +24,68 @@ console.log(entree);
 entree = 'Tacos'; // this throws TypeError: Assignment to constant variable.
 const testing; // this throws SyntaxError: Missing initializer in const declaration
 */
+
+let u = 4;
+u = u + 1;
+console.log(u); // Output: 5
+
+let w = 4;
+w += 1;
+console.log(w); // Output: 5
+
+let x = 20;
+x -= 5; // Can be written as x = x - 5
+console.log(x); // Output: 15
+ 
+let y = 50;
+y *= 2; // Can be written as y = y * 2
+console.log(y); // Output: 100
+ 
+let z = 8;
+z /= 2; // Can be written as z = z / 2
+console.log(z); // Output: 4
+
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
+
+// Increment by 1
+let a = 10;
+a++;
+console.log(a); // Output: 11
+
+// Decrement by 1
+let b = 20;
+b--;
+console.log(b); // Output: 19
+
+// String concatenation
+let myPet = 'armadillo';
+console.log('I own a pet ' + myPet + '.'); 
+// Output: 'I own a pet armadillo.'
+
+// String interpolation
+const pet = 'armadillo';
+console.log(`I own a pet ${pet}.`);
+// Output: I own a pet armadillo.
+
+// typeof operator
+const unknown1 = 'foo';
+console.log(typeof unknown1); // Output: string
+ 
+const unknown2 = 10;
+console.log(typeof unknown2); // Output: number
+ 
+const unknown3 = true; 
+console.log(typeof unknown3); // Output: boolean
